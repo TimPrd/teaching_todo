8ebd825a91469e6f9680b35419a49e937c8309e0
TimPrd <timothe.pardieu@outlook.com>
2020-02-18 09:46:46 +0100

    kibana

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const Todo = require("./models/todo");
+const log = require("./log");
 
 const todoFront = fs.readFileSync("./src/front/todo.ejs", "utf-8");
 
@@ -15,36 +16,47 @@ mongoose.connect(process.env.DB || "mongodb://localhost/todo", { useNewUrlParser
     process.exit(1);
   }
   app.emit("db ready");
-})
+});
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/add_todo", async (req, res) => {
+  log.verbose("/add_todo", {
+    route: "/add_todo",
+  });
   if(req.body.todo) {
     await Todo.create({ content: req.body.todo });
 		res.status(201).redirect("/");
   } else {
+    log.error("cannot create empty todo")
     res.json({ error: "cannot create empty todo" });
   }
-})
+});
 
 app.post("/delete_todo/:id", async (req, res) => {
 	const id = req.params.id;
+  log.verbose("/delete_todo", {
+    route: "/delete_todo",
+    id,
+  });
 	if (id)
 		await Todo.findOneAndDelete({"_id": id})
 	res.status(200).redirect("/");
-})
+});
 
 app.get("/", async (req, res) => {
+  log.verbose("/", {
+    route: "/",
+  });
   const todos = await Todo.find({}, ["content", "_id"]);
   res.end(ejs.render(todoFront, { todos }));
-})
+});
 
 
 app.on("db ready", () => {
   const PORT = process.env.PORT || 4000;
-  console.log(`app listening on port ${PORT}`);
+  log.info("app listening", {PORT});
   app.listen(PORT);
 });
 
